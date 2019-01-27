ac87586b9ae9721549f96bba4768813901200742
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-01-27 21:48:10 +0200

    Add provider related configuration

---
+++ weatherapp/rp5/config.py
@@ -0,0 +1,11 @@
+# rp5.ua provider related configuration
+RP5_PROVIDER_NAME = 'rp5'            # provider id
+RP5_PROVIDER_TITLE = 'rp5.ua'        # provider title
+
+DEFAULT_RP5_LOCATION_NAME = 'Lviv'
+DEFAULT_RP5_LOCATION_URL = \
+    ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D1%83_%D0%9B%D1%8C%'
+     'D0%B2%D0%BE%D0%B2%D1%96,_%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%'
+     'BA%D0%B0_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C')
+RP5_BROWSE_LOCATIONS = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_'
+                        '%D0%B2_%D1%81%D0%B2%D1%96%D1%82%D1%96')
